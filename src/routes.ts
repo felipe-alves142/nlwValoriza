fc8ba1334118293ceea3ac5c10672ef46074b511
felipe-alves142 <77590921+felipe-alves142@users.noreply.github.com>
2021-06-26 13:50:59 -0300

    Aula 3 e 4

---
+++ src/routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateTagController } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
+const router = Router();
+
+const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
+const createComplimentsController = new CreateComplimentController();
+
+router.post("/tags", ensureAdmin, createTagController.handle);
+router.post("/users", createUserController.handle);
+router.post("/login", authenticateUserController.handle);
+router.post("/compliments", createComplimentsController.handle);
+
+export { router };
